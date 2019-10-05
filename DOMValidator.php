9edc3a887b92d4cb87b8da24223a4f205807b2d9
ravikumar-raja <41184458+ravikumar-raja@users.noreply.github.com>
2019-10-05 23:14:53 +0530

    Update DOMValidator.php

--- DOMValidator.php
+++ DOMValidator.php
@@ -25,65 +25,61 @@ class DOMValidator {
 	 * return error message
 	 */
 	private function libXMLDisplayError($error) : string
-    {
-        $errorString = "Error $error->code in $error->file (Line:{$error->line}):";
-        $errorString .= trim($error->message);
-        return $errorString;
-    }
+    	{
+        	$errorString = "Error $error->code in $error->file (Line:{$error->line}):";
+        	$errorString .= trim($error->message);
+        	return $errorString;
+    	}
 	
 	/**
 	 * Fetch libxml errors
 	 * @return error message
 	 */		
 	private function libXMLDisplayErrors() : array
-    {
-        $errors = libxml_get_errors();
+    	{
+        	$errors = libxml_get_errors();
 		$result    = [];
-        foreach ($errors as $error) {
-            $result[] = $this->libXMLDisplayError($error);
-        }
+       		foreach ($errors as $error) {
+            		$result[] = $this->libXMLDisplayError($error);
+        	}
 
-        libxml_clear_errors();
-        return $result;
-    }	
+        	libxml_clear_errors();
+        	return $result;
+    	}	
 
 	/**
 	 * Function to validate feeds on several conditions
 	 * @return false if non-validated, true if validated.
 	 */	
 	public function validateFeeds($feeds) : string
-    {
-       if (!class_exists('DOMDocument')) {
+    	{
+       		if (!class_exists('DOMDocument')) {
 			$this->errorDetails = 'Schema is Missing, Please add schema to feedSchema property';
-            
-        }
-        if (!file_exists($this->feedSchema)) {
-			$this->errorDetails = 'Schema is Missing, Please add schema to feedSchema property';
-			
-        }
-        libxml_use_internal_errors(true);
-       
-       
-        $this->handler->loadXML($feeds, LIBXML_NOBLANKS);
+        	}
+        	if (!file_exists($this->feedSchema)) {
+			$this->errorDetails = 'Schema is Missing, Please add schema to feedSchema property';	
+        	}
+        	libxml_use_internal_errors(true);
+        	$this->handler->loadXML($feeds, LIBXML_NOBLANKS);
 		$this->handler->saveXML();
 		
 		//Condition to check if schema is validated
-        if (!$this->handler->schemaValidate($this->feedSchema)) {
-           $this->errorDetails = $this->libXMLDisplayErrors();
-		   return false;		   
-        } 
+        	if (!$this->handler->schemaValidate($this->feedSchema)) {
+           		$this->errorDetails = $this->libXMLDisplayErrors();
+		   	return false;		   
+        	} 
 		else {
-           return true;
-        }
-    }
+           		return true;
+        	}
+    	}
 	
 	/**
 	 * Pull error message after validating the feed 
 	 * @return error message
 	 */	
 	public function displayErrors()
-    {
-        return $this->errorDetails;
-    }		
+    	{
+    	    return $this->errorDetails;
+    	}		
 	
 }
